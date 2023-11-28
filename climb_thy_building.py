f8d6159f7dd29ddf57e05600f5e541ffb0f6906f
VinuthaTJ179 <93989675+VinuthaTJ179@users.noreply.github.com>
2023-11-28 23:11:11 +0530

    Update climb_thy_building.py

--- climb_thy_building.py
+++ climb_thy_building.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 import time
-
+import button
 # Initialize Pygame
 pygame.init()
 
@@ -13,10 +13,21 @@ screen_height = 1080
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pygame Text and Image Animation")
 
-# Load the image
+start_img = pygame.image.load("C:\\Users\Vinutha TJ\\OneDrive\\Desktop\\GameOff2023\\start_img.png").convert_alpha()
+exit_img = pygame.image.load("C:\\Users\Vinutha TJ\\OneDrive\\Desktop\\GameOff2023\\exit_img.png").convert_alpha()
+
+
+
+
+#Create button instances
+
+start_button = button.Button(840,450,start_img,0.8)
+exit_button = button.Button(855,600,exit_img,0.8)
+
+# Load the images
 splash_image = pygame.image.load("C:\\Users\\Vinutha TJ\\OneDrive\\Desktop\\GameOff2023\\cityskyline.png")
 second_screen_image = pygame.image.load("C:\\Users\\Vinutha TJ\\OneDrive\\Desktop\\GameOff2023\\sunnyday.png")
-# Set the initial position of the image
+
 x, y = 0, 0
 
 # Define a font and initial text
@@ -29,26 +40,29 @@ text_surface = font.render('', True, text_color)
 text_x = 610
 text_y = 500
 
-#*********************************************************************************
+# *********************************************************************************
 SPLASH_SCREEN = 0
 SECOND_SCREEN = 1
 current_state = SPLASH_SCREEN
 
 # Timer to control the splash screen duration
-transition_duration = 7000  # Duration of the transition in milliseconds (7 seconds)
+transition_duration = 5000  # Duration of the transition in milliseconds (5 seconds)
 start_time = pygame.time.get_ticks()
 alpha = 255  # Initial alpha value for fading (fully opaque)
-fade_speed = 2  
+fade_speed = 2
 
-#*********************************************************************************
-
-
+# *********************************************************************************
 
 # Initialize variables for the animation
 animation_text = "Climb thy building"
-animation_speed = 5  # Number of letters displayed per second
+animation_speed = 7  # Number of letters displayed per second
 current_letter_index = 0
 time_last_letter_displayed = time.time()
+
+
+
+
+
 
 # Main game loop
 running = True
@@ -59,6 +73,8 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+
+
     if current_state == SPLASH_SCREEN:
         # Clear the screen
         screen.fill((0, 0, 0))  # Black background
@@ -74,19 +90,22 @@ while running:
 
         # Display the letters one by one with the defined animation speed
         if current_letter_index < len(animation_text) and time_since_last_display >= 1 / animation_speed:
-           text_surface = font.render(animation_text[:current_letter_index + 1], True, text_color)
-           time_last_letter_displayed = time_now
-           current_letter_index += 1
+            text_surface = font.render(animation_text[:current_letter_index + 1], True, text_color)
+            time_last_letter_displayed = time_now
+            current_letter_index += 1
 
         # Blit (draw) the text surface at the center of the screen
         screen.blit(text_surface, (text_x, text_y))
     elif current_state == SECOND_SCREEN:
-        # Clear the screen
         screen.fill((0, 0, 0))  # Black background
 
         # Blit (draw) the second screen image
         screen.blit(second_screen_image, (x, y))
-    
+        if start_button.draw(screen) == True:
+            print('START')
+        if exit_button.draw(screen) == True:
+            running = False
+
         if alpha > 0:
             overlay = pygame.Surface((screen_width, screen_height))
             overlay.set_alpha(alpha)
